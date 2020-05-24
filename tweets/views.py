dc6d3d62382511b012639ffa0ef9812c95c14651
Niidarku <niidarku66@gmail.com>
2020-05-24 15:36:28 +0000

    created views for the forms.py

--- tweets/views.py
+++ tweets/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
+
+from .forms import TweetForm
+
 def home_view(request):
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, "components/forms.html", context={"form":form})
+
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()    
